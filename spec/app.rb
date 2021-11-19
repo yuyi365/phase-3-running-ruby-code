77ec612c9947c5a1fc7022f7ac41da86bacd4fd0
Yuyi <yuyi365@gmail.com>
2021-11-18 22:45:19 -0500

    commit tree ff25d1838670cd0bdeb26c50ffc98119f9ce2b0b

---
+++ spec/app.rb
@@ -0,0 +1,17 @@
+puts "Hello World"
+puts "Hello World"
+puts "Hello World"
+
+print "Hello World"
+print "Hello World"
+print "Hello World"
+
+p [ 1,2,3 ]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1,2,3]
